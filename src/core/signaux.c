d0f269829e92c5c6308568ff146ed9705e60fdc3
fiaudfiz <michelouali0@gmail.com>
2026-06-09 10:36:54 +0200

    perf : upgrade on signaux.c : change of fonctions from sigaction() to signal()

--- src/core/signaux.c
+++ src/core/signaux.c
@@ -1,23 +1,16 @@
-#define _POSIX_C_SOURCE 200809L
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
 
-/*struct sigaction {
-    void     (*sa_handler)(int);   // handler ou SIG_IGN ou SIG_DFL
-    sigset_t sa_mask;              // signaux bloqués pendant le handler
-    int      sa_flags;             // options
-    void     (*sa_restorer)(void); // obsolète, ignore
-};*/
-
-
-struct  sigaction sa;
+void    handle_sigint(int sig)
+{
+    (void)sig;
+    write(1, "\n", 1);
+    // réafficher le prompt si besoin
+}
 
 void    set_signaux_interactif(void)
 {
-    sigemptyset(&sa.sa_mask);
-    sa.__sigaction_handler = handle_sigint(); //fonction a faire
-    sigaction(SIGINT, &sa, NULL);
-    sa.__sigaction_handler = SIG_IGN; //ignorer en mode interactif
-    sigaction(SIGQUIT, &sa, NULL);
+    signal(SIGINT, handle_sigint);
+    signal(SIGQUIT, SIG_IGN);
 }
