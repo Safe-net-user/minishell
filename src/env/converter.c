c62618642abb2a22ba9b144cf274cf701a896ecc
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-06-11 15:22:53 +0200

    refactor(env): env files logic refactored

--- src/env/converter.c
+++ src/env/converter.c
@@ -1,33 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_env.c                                         :+:      :+:    :+:   */
+/*   converter.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gd-hallu <gd-hallu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/10 17:05:34 by gd-hallu          #+#    #+#             */
-/*   Updated: 2026/06/10 19:47:42 by gd-hallu         ###   ########.fr       */
+/*   Created: 2026/06/10 14:01:35 by gd-hallu          #+#    #+#             */
+/*   Updated: 2026/06/11 14:31:33 by gd-hallu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "env.h"
+#include "ft_strings.h"
+#include <stdio.h>
 
-void    free_env(t_env *env)
+int env_arr_to_ht(char **envp, t_env *ht)
 {
-    size_t  i;
-
-    i = 0;
-    while (i < env->capacity)
-    {
-        if (env->indexes[i].key != NULL  && env->indexes[i].key != DELETED)
-        {
-            free(env->indexes[i].value);
-            free(env->indexes[i].key);
-        }
-        i++;
-    }
-    free(env->indexes);
-    free(env);
-    env = NULL;
+	size_t		i;
+	size_t		j;
+	int			flags;
+	char		*start;
+	
+	i = 0;
+	flags = 0;
+	while(envp[i])
+	{
+        j = 0;
+		start = ft_strdup(envp[i]);
+		while (start[j] != '=')
+			j++;
+		start[j] = '\0';
+		j++;
+		add_env(ht, start, &start[j], flags |= EXPORTED);
+		i++;
+	}
+	return (1);
 }
---
+++ src/env/set_env.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_env.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gd-hallu <gd-hallu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/11 13:49:27 by gd-hallu          #+#    #+#             */
+/*   Updated: 2026/06/11 15:05:36 by gd-hallu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "ft_stdlib.h"
+#include "env.h"
+#include <unistd.h>
+
+
+/** 
+ * @brief Initializes environment hash table from envp.
+ * 
+ * @note Ensures mandatory variables (_ , SHLVL, PWD) exist.
+ * 
+ * @param mms Mini-shell context structure
+ * @param envp Null-terminated environment array
+ * 
+*/
+int	set_exported_env_ht(t_mms *mms, char **envp)
+{
+	t_env_entry	*p;
+	int			shell_lvl;
+	
+	if (env_arr_to_ht(envp, mms->env) == 0)
+		return (0);
+	if (!get_env(mms->env, "_"))
+		add_env(mms->env, "_", "/usr/bin/env", EXPORTED);
+	p = get_env(mms->env, "SHLVL");
+	if (!p)
+		add_env(mms->env, "SHLVL", "1", EXPORTED);
+	else
+	{
+		shell_lvl = ft_atoi(p->value) + 1;
+		add_env(mms->env, "SHLVL", ft_itoa(shell_lvl), EXPORTED);
+	}
+	if (!get_env(mms->env, "PWD"))
+		add_env(mms->env, "PWD", getcwd(NULL, 0), EXPORTED);
+	return (1);
+}
+
+/** 
+ * @brief Add intern shell variable in environment hash table.
+ * 
+ * @note Ensures PATH exist.
+ * 
+ * @param mms Mini-shell context structure
+ * 
+*/
+int	set_var_env_ht(t_mms *mms)
+{	
+	add_env(mms->env, "PS1", "'\\s-\\v\\$'", 0);
+	add_env(mms->env, "PS2", "'> '", 0);
+	add_env(mms->env, "PS4", "'+ '", 0);
+	if (!get_env(mms->env, "PATH"))
+		add_env(mms->env, "PATH", "/usr/local/bin:/usr/bin", 0);
+	return (1);
+}
