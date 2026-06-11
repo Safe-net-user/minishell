77c421d2052b494706cee4a88dfc9bf4d0453fbf
fiaudfiz <michelouali0@gmail.com>
2026-06-11 21:23:57 +0200

    feat : add the global varaible in signaux.c

--- src/core/signaux.c
+++ src/core/signaux.c
@@ -1,12 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signaux.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fiaudfiz <fiaudfiz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/11 21:18:50 by fiaudfiz          #+#    #+#             */
+/*   Updated: 2026/06/11 21:18:51 by fiaudfiz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "minishell.h"
+
+int g_signal = 0;
 
 void    handle_sigint(int sig)
 {
-    (void)sig;
-    write(1, "\n", 1);
-    // réafficher le prompt si besoin
+    g_signal = sig;
+    write(1, "\nminiMishell$:", 14);
 }
 
 void    set_signaux_interactif(void)
