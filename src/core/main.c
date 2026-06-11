a4fdb2ccb15b9c251557b9ec9556c53c487b6d98
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-06-11 15:25:20 +0200

    chore(main): moved functions and added allocation handling logic

--- src/core/main.c
+++ src/core/main.c
@@ -6,7 +6,7 @@
 /*   By: gd-hallu <gd-hallu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 10:30:51 by fiaudfiz          #+#    #+#             */
-/*   Updated: 2026/06/10 19:43:52 by gd-hallu         ###   ########.fr       */
+/*   Updated: 2026/06/11 15:05:11 by gd-hallu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "utils.h"
 #include "minishell.h"
 #include "ft_memory.h"
 #include "ft_string_builder.h"
 #include "ft_strings.h"
-#include "ft_stdlib.h"
+#include "env.h"
 
 /**
  * A noter que si HOME est change, alors la ht des commandes doit etre videe.
  * Penser a revenir sur cette taille car on risque largement de cache miss. (n x sizeof(type))
  * Penser aux conditions si une allocation rate.
  */
-static	int	init_og_struct(t_mms *mms)
+static	t_mms	*init_og_struct(void)
 {
+	t_mms	*mms;
+	
 	mms = malloc(sizeof(t_mms));
 	if (!mms)
 		return (0);
@@ -36,95 +37,36 @@ static	int	init_og_struct(t_mms *mms)
 	mms->alias = init_hash_table(INIT_SIZE_HT);
 	mms->cmd_path = init_hash_table(INIT_SIZE_HT);
 	mms->sa = init_stack_allocator(INIT_SIZE_SA);
-	return (1);
-}	
-
-/** 
- * @brief Initializes environment hash table from envp.
- * 
- * @note Ensures mandatory variables (_ , SHLVL, PWD) exist.
- * 
- * @param mms Mini-shell context structure
- * @param envp Null-terminated environment array
- * 
-*/
-static int	set_exported_env_ht(t_mms *mms, char **envp)
-{
-	t_env_entry	*p;
-	int			shell_lvl;
-	t_env_flags	flags;
-	
-	flags = 0;
-	if (arr_to_ht(envp, mms->env) == 0)
-		return (0);
-	if (!get_env(mms->env, "_"))
-		add_env(mms->env, "_", "/usr/bin/env", flags |= EXPORTED);
-	p = get_env(mms->env, "SHLVL");
-	if (!p)
-		add_env(mms->env, "SHLVL", "1", flags |= EXPORTED);
-	else
-	{
-		shell_lvl = ft_atoi(p->value) + 1;
-		add_env(mms->env, "SHLVL", ft_itoa(shell_lvl), flags |= EXPORTED);
-	}
-	if (!get_env(mms->env, "PWD"))
-		add_env(mms->env, "PWD", getcwd(NULL, 0), flags |= EXPORTED);
-	return (1);
-}
-
-/** 
- * @brief Add intern shell variable in environment hash table.
- * 
- * @note Ensures PATH exist.
- * 
- * @param mms Mini-shell context structure
- * 
-*/
-static int	set_env_ht(t_mms *mms)
-{
-	t_env_flags	flags;
-	
-	flags = 0;
-	add_env(mms->env, "PS1", "'\\s-\\v\\$'", flags);
-	add_env(mms->env, "PS2", "'> '", flags);
-	add_env(mms->env, "PS4", "'+ '", flags);
-	if (!get_env(mms->env, "PATH"))
-		add_env(mms->env, "PATH", "/usr/local/bin:/usr/bin", flags);
-	return (1);
+	mms->cwd = getcwd(NULL, 0);
+	mms->name = ft_strdup("miniMishell");
+	mms->last_status = 0;
+	mms->umask = 0022;
+	if (!mms->env || !mms->alias || !mms->cmd_path || !mms->sa || mms->name)
+		return (NULL);
+	return (mms);
 }
 
 static	int set_og_struct(t_mms *mms, char **envp)
 {
+	if (!mms || !envp)
+		return (0);
 	if (set_exported_env_ht(mms, envp) == 0)
 		return (0);
-	if (set_env_ht(mms) == 0)
+	if (set_var_env_ht(mms) == 0)
 		return (0);
 	return (1);
 }
-
-/*
- *Si dans env il manque des variables comme PWD, OLDPWD, PATH etc il faudra aux moment cle les rajouter dans la ht var et non pas env
- 
-static	int	set_var_ht(t_mms *mms, char **envp)
-{
-	ft_memcpy(mms->var->entries, mms->env->entries, (INIT_SIZE_HT * sizeof(t_entry)));
-	set_new_value(mms->var, "PS1", "'\\s-\\v\\$'");
-	set_new_value(mms->var, "PS2", "'> '");
-	set_new_value(mms->var, "PS4", "'+ '");
-}*/
 
 int main(UNUSED int ac, UNUSED char **av, char **envp)
 {
 	//char 	*result;
 	t_mms	*mms;
-
-	mms = NULL;
-	if (init_og_struct(mms) == 0)
-		return (1);
-	else if (set_og_struct(mms, envp) == 0)
+	
+	mms = init_og_struct();
+	if (!mms || set_og_struct(mms, envp) == 0)
 	{
-		//free_og_struct(mms);
-		return (1);
+		free_og_struct(mms);
+		return (EXIT_FAILURE);
 	}
 	/*while (1) //de ici
 	{
