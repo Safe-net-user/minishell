bd3694dbfbb3b04c4b07a17703224fbb9d841f31
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-06-12 11:57:45 +0200

    chore(Doc): Add Doxygene comment to main strcuture

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   converter.c                                        :+:      :+:    :+:   */
+/*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gd-hallu <gd-hallu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/10 14:01:35 by gd-hallu          #+#    #+#             */
-/*   Updated: 2026/06/12 11:02:24 by gd-hallu         ###   ########.fr       */
+/*   Created: 2026/06/12 11:33:11 by gd-hallu          #+#    #+#             */
+/*   Updated: 2026/06/12 11:52:24 by gd-hallu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "env.h"
-#include "ft_strings.h"
-#include <stdio.h>
+#include "minishell.h"
 
-int env_arr_to_ht(char **envp, t_env *ht)
+void    lexer(char *cmdl, t_mms *mms)
 {
-	size_t		i;
-	size_t		j;
-	char		*start;
-	
-	i = 0;
-	while(envp[i])
-	{
-        j = 0;
-		start = ft_strdup(envp[i]);
-		while (start[j] != '=')
-			j++;
-		start[j] = '\0';
-		j++;
-		add_env(ht, start, &start[j], EXPORTED);
-		i++;
-	}
-	return (1);
+    
 }
