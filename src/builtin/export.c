f14d56ecc811bae2b84fe832bcb87ed2f8808617
fiaudfiz <michelouali0@gmail.com>
2026-06-13 11:36:56 +0200

    feat : export.c

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: fiaudfiz <fiaudfiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 11:42:49 by fiaudfiz          #+#    #+#             */
-/*   Updated: 2026/06/12 13:39:14 by fiaudfiz         ###   ########.fr       */
+/*   Updated: 2026/06/13 11:11:26 by fiaudfiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 // export VAR=valeur → crée/modifie + flag EXPORTED
 // export VAR → flag EXPORTED sur variable existante
 // dans env flags on a un EXPORT 1 ou 0
+
 int     export(t_env *env, char *arg)
 {
     if (!arg)
