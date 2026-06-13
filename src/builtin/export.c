b5f10e4d54e360933b3b88efd5f8bff0c4f552b6
fiaudfiz <michelouali0@gmail.com>
2026-06-13 10:55:57 +0200

    feat : export.c

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: fiaudfiz <fiaudfiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 11:42:49 by fiaudfiz          #+#    #+#             */
-/*   Updated: 2026/06/11 19:02:30 by fiaudfiz         ###   ########.fr       */
+/*   Updated: 2026/06/12 13:39:14 by fiaudfiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 #include"env.h"
 
 //important, chaque nouvelle exportee vient au dessus de la liste et non en dessous.
-
+// export seul affiche toutes les variables exportees
+// export VAR=valeur → crée/modifie + flag EXPORTED
+// export VAR → flag EXPORTED sur variable existante
+// dans env flags on a un EXPORT 1 ou 0
 int     export(t_env *env, char *arg)
 {
     if (!arg)
