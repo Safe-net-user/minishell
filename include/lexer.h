e0ccd1b202edbaf7321f1a14b302abd2fa3ce6e4
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-06-15 11:12:40 +0200

    chore(header): add lexer header file with structs and function definitions

--- include/lexer.h
+++ include/lexer.h
@@ -6,12 +6,19 @@
 /*   By: gd-hallu <gd-hallu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 16:49:01 by gd-hallu          #+#    #+#             */
-/*   Updated: 2026/06/11 16:56:55 by gd-hallu         ###   ########.fr       */
+/*   Updated: 2026/06/15 11:05:29 by gd-hallu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_H
 # define LEXER_H
+
+#include "minishell.h"
+#include "ft_string_builder.h"
+#include <stdbool.h>
+
+# define BUFFER_TOKEN 2048
+# define INIT_SIZE_SB 64
 
 typedef enum e_type_token {
     TOK_WORD,
@@ -44,27 +51,28 @@ typedef enum e_type_token {
 }	t_type_token;
 
 typedef enum e_flag_token {
-    SQUOTED = 1 << 0,
-    DQUOTED = 1 << 1,
-    BACK_TICK = 1 << 2,
-    ARITH = 1 << 3,
-    COMMENT = 1 << 4,
-    ESCAPED = 1 << 5,
-    GLOB = 1 << 6,
-    TILDE = 1 << 7
+    TOKF_SQUOTED = 1 << 0,
+    TOKF_DQUOTED = 1 << 1,
+    TOKF_BACK_TICK = 1 << 2,
+    TOKF_ARITH = 1 << 3,
+    TOKF_COMMENT = 1 << 4,
+    TOKF_ESCAPED = 1 << 5,
+    TOKF_GLOB = 1 << 6,
+    TOKF_TILDE = 1 << 7,
 }   t_flag_token;
 
 typedef enum e_state_lexer {
-	NORMAL = 1 << 0,
-	SQUOTED = 1 << 1,
-	DQUOTED = 1 << 2,
-	BACK_TICK = 1 << 3,
-	ARITH = 1 << 4,
-	COMMENT = 1 << 5,
-	OPERATOR = 1 << 6,
-	ESCAPED = 1 << 7,
-	HERE_DOC = 1 << 8,
-    EXPENSION = 1 << 9,
+	ST_NORMAL = 1 << 0,
+	ST_SQUOTED = 1 << 1,
+	ST_DQUOTED = 1 << 2,
+	ST_BACK_TICK = 1 << 3,
+	ST_ARITH = 1 << 4,
+	ST_COMMENT = 1 << 5,
+	ST_OPERATOR_AND = 1 << 6,
+    ST_OPERATOR_OR = 1 << 7,
+	ST_ESCAPED = 1 << 8,
+	ST_HERE_DOC = 1 << 9,
+    ST_EXPENSION = 1 << 10,
 } t_state_lexer;
 
 typedef struct s_token {
@@ -72,4 +80,8 @@ typedef struct s_token {
 	t_flag_token	flags;
 	t_type_token	type_tk;
 }   t_token;
+
+int    lexer(char *cmdl, t_mms *mms);
+bool    rule_op_continue(t_mms *mms, int *state, char *cmdl, t_sb *sb);
+
 #endif
