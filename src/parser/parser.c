e7252e4e6aa469f358b5112b0f89048198bb2bfc
fiaudfiz <michelouali0@gmail.com>
2026-06-25 17:02:33 +0200

    docs : add some comment in parser.c

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: fiaudfiz <fiaudfiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 10:27:18 by fiaudfiz          #+#    #+#             */
-/*   Updated: 2026/06/25 12:09:19 by fiaudfiz         ###   ########.fr       */
+/*   Updated: 2026/06/25 16:58:28 by fiaudfiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,23 @@ t_token *next_token(t_token *cur)
     h = (t_header *)cur - 1;
     return ((t_token *)((uint8_t *)cur + h->size + sizeof(t_header)));
 }
+
+/**
+ * @brief Parse a simple shell command into an AST node.
+ *
+ * This function creates a NODE_CMD node and consumes all consecutive
+ * WORD tokens. Each word is stored in the argv array of the command.
+ *
+ * The function stops parsing when it reaches an operator token
+ * such as a pipe, AND, OR, or the end of the token stream.
+ *
+ * @param mms Main minishell structure containing the stack allocator.
+ * @param token Pointer to the current token pointer.
+ *              The pointer is updated as tokens are consumed.
+ *
+ * @return Pointer to the created command AST node.
+ */
+
 
 t_ast *parse_command(t_mms *mms, t_token **token)
 {
@@ -44,6 +61,34 @@ t_ast *parse_command(t_mms *mms, t_token **token)
     return (node);
 }
 
+/**
+ * @brief Parse a pipeline expression and build PIPE nodes.
+ *
+ * This function parses commands separated by pipe operators.
+ *
+ * Example:
+ *
+ *     ls | grep txt | wc
+ *
+ * becomes a left-associative AST:
+ *
+ *          |
+ *         / \
+ *        |   wc
+ *       / \
+ *      ls grep
+ *
+ * Each pipe node stores the previous parsed tree as its left child
+ * and the next command as its right child.
+ *
+ * @param mms Main minishell structure containing the stack allocator.
+ * @param token Pointer to the current token pointer.
+ *              The pointer advances while parsing.
+ *
+ * @return Pointer to the root of the generated pipeline AST.
+ */
+
+
 t_ast *parse_pipe(t_mms *mms, t_token **token)
 {
     t_ast   *node;
@@ -66,6 +111,32 @@ t_ast *parse_pipe(t_mms *mms, t_token **token)
     }
     return (left);
 }
+
+/**
+ * @brief Parse logical operators (&& and ||).
+ *
+ * This function parses expressions containing logical operators.
+ * It works on top of the pipeline parser to respect shell precedence:
+ *
+ *     command -> pipeline -> AND/OR
+ *
+ * Example:
+ *
+ *     echo a && cat file | grep x
+ *
+ * becomes:
+ *
+ *          &&
+ *         /  \
+ *       echo  |
+ *            / \
+ *          cat grep
+ *
+ * @param mms Main minishell structure containing the stack allocator.
+ * @param token Pointer to the current token pointer.
+ *
+ * @return Pointer to the root of the AND/OR AST.
+ */
 
 t_ast *parse_or_and(t_mms *mms, t_token **token)
 {
@@ -90,6 +161,27 @@ t_ast *parse_or_and(t_mms *mms, t_token **token)
     }
     return (left);
 }
+
+/**
+ * @brief Entry point of the shell parser.
+ *
+ * Initializes the token iterator from the stack allocator buffer
+ * and starts parsing from the highest grammar level.
+ *
+ * Parsing order:
+ *
+ *     COMMAND
+ *        |
+ *      PIPE
+ *        |
+ *      AND / OR
+ *
+ * The returned AST represents the complete shell command structure.
+ *
+ * @param mms Main minishell structure.
+ *
+ * @return Pointer to the root AST node.
+ */
 
 t_ast *parser(t_mms *mms)
 {
