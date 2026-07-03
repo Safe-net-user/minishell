e76d3cb1dcedf1f40c04c4ec42f027e96130b806
Michel <michelouali0@gmail.com>
2026-07-03 15:53:12 +0200

    feat(parser): test integration

---
+++ tests/integration/integration.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   integration.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/03 15:18:29 by miouali           #+#    #+#             */
+/*   Updated: 2026/07/03 15:22:28 by miouali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INTEGRATION_H
+#define INTEGRATION_H
+
+# include "../../include/parser.h"
+# include "../../include/minishell.h"
+# include <stdio.h>
+
+#endif
