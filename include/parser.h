65f8799550b3cdff700b747926eefb814d89d7df
fiaudfiz <michelouali0@gmail.com>
2026-07-21 00:23:48 +0200

    fix(parser): mise correcte de la struct node pour l'expander de monsieur gaspard

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: fiaudfiz <fiaudfiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 10:27:34 by fiaudfiz          #+#    #+#             */
-/*   Updated: 2026/07/20 13:20:34 by fiaudfiz         ###   ########.fr       */
+/*   Updated: 2026/07/20 23:42:06 by fiaudfiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ typedef enum e_node_type
 
 typedef struct s_redir
 {
-    t_type_token    type;
+    t_type_tk    type_tk;
     char            *file;
     struct s_redir  *next;
 }   t_redir;
@@ -37,19 +37,17 @@ typedef struct s_redir
 typedef struct s_ast
 {
     t_node_type     type;       // type du noeud AST
-    t_type_token    tok_type;   // type du token lexer (TOK_LESS, TOK_GREAT, etc.)
-    t_flag_token    *flags;      // flags du token lexer (SQUOTED, DQUOTED, etc.)
     struct s_ast    *left;
     struct s_ast    *right;
     t_redir   *redirect;
     union
     {
-        char            **argv;
+        t_tk    **tokens;
         //t_ast *subshell;
     };
 }   t_ast;
 
 t_ast *parser(t_mms *mms);
-t_token *next_token(t_token *cur);
+t_tk *next_token(t_tk *cur);
 
 #endif
