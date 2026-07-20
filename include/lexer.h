973fdda0e28d36386951f84dca57978a48c79737
fiaudfiz <michelouali0@gmail.com>
2026-07-21 00:24:11 +0200

    chore(parser): update lexer.h

--- include/lexer.h
+++ include/lexer.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fiaudfiz <fiaudfiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 16:49:01 by gd-hallu          #+#    #+#             */
-/*   Updated: 2026/06/18 15:24:26 by miouali          ###   ########.fr       */
+/*   Updated: 2026/07/20 23:28:00 by fiaudfiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@
 #include <stdbool.h>
 
 # define BUFFER_TOKEN 2048
-# define INIT_SIZE_SB 64
+# define DEFAULT_SB_SIZE 64
 
-typedef enum e_type_token {
+typedef enum e_type_tk
+{
     TOK_WORD,
     TOK_COMMAND,
     TOK_ASSIGNMENT_WORD,
@@ -28,6 +29,7 @@ typedef enum e_type_token {
     TOK_IO_NUMBER,
     TOK_IO_LOCATION,
     TOK_RESERVED_WORD,
+    TOK_DELIMITER,
     TOK_PIPE,
     TOK_AND_IF,         // &&
     TOK_OR_IF,          // ||
@@ -48,40 +50,64 @@ typedef enum e_type_token {
     TOK_RPAREN,         // )
     TOK_NEWLINE,
     TOK_EOF
-}	t_type_token;
+}	t_type_tk;
 
-typedef enum e_flag_token {
-    TOKF_SQUOTED = 1 << 0,
-    TOKF_DQUOTED = 1 << 1,
+typedef enum e_flag_tk
+{
+    TOKF_SQUOTE = 1 << 0,
+    TOKF_DQUOTE = 1 << 1,
     TOKF_BACK_TICK = 1 << 2,
     TOKF_ARITH = 1 << 3,
     TOKF_COMMENT = 1 << 4,
-    TOKF_ESCAPED = 1 << 5,
-    TOKF_GLOB = 1 << 6,
-    TOKF_TILDE = 1 << 7,
-}   t_flag_token;
+    TOKF_ESCAPE = 1 << 5,
+    TOKF_SPEC_PARAM = 1 << 6,
+    TOKF_EXPANSION = 1 << 7,
+    TOKF_TILDE = 1 << 8,
+}   t_flag_tk;
 
-typedef enum e_state_lexer {
-	ST_NORMAL = 1 << 0,
-	ST_SQUOTED = 1 << 1,
-	ST_DQUOTED = 1 << 2,
-	ST_BACK_TICK = 1 << 3,
-	ST_ARITH = 1 << 4,
-	ST_COMMENT = 1 << 5,
-	ST_OPERATOR_AND = 1 << 6,
-    ST_OPERATOR_OR = 1 << 7,
-	ST_ESCAPED = 1 << 8,
-	ST_HERE_DOC = 1 << 9,
-    ST_EXPENSION = 1 << 10,
-} t_state_lexer;
+typedef enum e_state_lx
+{
+	LX_NORMAL,
+	LX_SQUOTE,
+	LX_DQUOTE,
+	LX_OPERATOR,
+} t_state_lx;
 
-typedef struct s_token {
-	char		    *value;
-	t_flag_token	flags;
-	t_type_token	type_tk;
-}   t_token;
+typedef enum e_val_lx
+{
+    LX_ERROR,
+    LX_SUCCESS,
+    LX_DQUOTE_NF,
+    LX_SQUOTE_NF,
+    LX_BQUOTE_NF,
+} t_val_lx;
 
-int    lexer(char *cmdl, t_mms *mms);
-bool    rule_op_continue(t_mms *mms, int *state, char *cmdl, t_sb *sb);
+typedef struct s_tk {
+    char		    *value;
+    t_flag_tk	flags;
+    t_type_tk	type_tk;
+}   t_tk;
+
+typedef struct s_lx
+{
+    t_mms   *mms;
+    char    *cmdl;
+    t_tk *tk;
+    t_sb    *sb;
+    size_t  index;
+    int     state;
+    bool    is_next_delimiter;
+}   t_lx;
+
+typedef t_val_lx (*state_lx_fn)(t_lx *);
+
+t_val_lx    lexer(char *cmdl, t_mms *mms);
+int         emit_tk(t_lx *lx);
+t_lx        *init_s_lx(char *cmdl, t_mms *mms);
+
+t_val_lx    lx_normal(t_lx *lx);
+t_val_lx    lx_squote(t_lx *lx);
+t_val_lx    lx_dquote(t_lx *lx);
+t_val_lx    lx_operator(t_lx *lx);
 
 #endif
