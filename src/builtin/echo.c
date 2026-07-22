e605845d402b3e9a96c72c41de38f23c1f14d02b
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-07-22 20:52:15 +0200

    refactor(echo): all builtin echo source code re created

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -14,69 +14,22 @@
 #include <stdio.h>
 #include <string.h>
 
-static int check_flag(char **str)
+t_builts_val builtin_echo(char **tab)
 {
+    bool new_line_active;
     int i;
-    int j;
-    int count_flag = 0;
-    
-    i = 2;
-    j = 0;
-    while (str[i])
-    {
-        while (str[i][j])
-        {
-            if (str[i][j] == '-')
-            {
-                j++;
-                if (str[i][j] == 'n')
-                {    
-                    while (str[i][j])
-                    {
-                        if (str[i][j] != 'n')
-                            return (count_flag);
-                        j++;
-                    }
-                    count_flag++;
-                }
-                else
-                    return (count_flag);
-            }
-            else
-                return (count_flag);
-        }
-        i++;
-    }
-    return (count_flag);
-}
 
-int     builtin_echo(char **tab)
-{
-    int i;
-    int flags;
-
-    flags = check_flag(tab);
-    i = 2 + flags;
+    if (!tab)
+        return (ERROR);
+    new_line_active = 1;
+    i = 1;
+    if (!tab[1])
+        write(1, "\n", 1);
+    if (ft_strcmp(tab[1], "-n") == 0)
+        new_line_active = 0; i++;
     while (tab[i])
-    {
-        printf("%s", tab[i]);
-        i++;
-    }
-    if (flags == 0)
-        printf ("\n");
-    return (0);
-}
-
-int main (int ac, char **av)
-{
-    if (ac < 2)
-    {
-        return (1);
-    }
-    if (strncmp(av[1], "echo", 5) == 0)
-    {
-        builtin_echo(av);
-    }
-    else
-        printf ("caca");
+        write(1, tab[i], ft_strlen(tab[i++]));
+    if (new_line_active)
+        write(1, "\n", 1);
+    return (SUCCESS);
 }
