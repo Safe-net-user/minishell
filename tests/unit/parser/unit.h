97b09b86d47c5c7946ea28e27ed2dfcd1a60aa81
fiaudfiz <michelouali0@gmail.com>
2026-07-23 15:11:24 +0200

    chore(parser): update on tester unit

--- tests/unit/parser/unit.h
+++ tests/unit/parser/unit.h
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   integration.h                                      :+:      :+:    :+:   */
+/*   unit.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fiaudfiz <fiaudfiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 15:18:29 by miouali           #+#    #+#             */
-/*   Updated: 2026/07/06 17:07:54 by miouali          ###   ########.fr       */
+/*   Updated: 2026/07/23 15:05:44 by fiaudfiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
     #ifndef UNIT_H
     #define UNIT_H
 
-    # include "../../include/parser.h"
-    # include "../../include/minishell.h"
+    # include "../../../include/parser.h"
+    # include "../../../include/minishell.h"
     # include <stdio.h>
 
     #endif
