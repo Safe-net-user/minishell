b6c062dcdac4b81db0614530772f0c8c70d98c34
fiaudfiz <michelouali0@gmail.com>
2026-07-23 13:18:48 +0200

    docs(exec): comment in exec.h

--- include/executor.h
+++ include/executor.h
@@ -6,36 +6,40 @@
 /*   By: fiaudfiz <fiaudfiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 11:23:14 by fiaudfiz          #+#    #+#             */
-/*   Updated: 2026/07/18 10:39:06 by fiaudfiz         ###   ########.fr       */
+/*   Updated: 2026/07/22 14:24:23 by fiaudfiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTOR_H
 # define EXECUTOR_H
 
-typedef struct s_executor
-{
-    pid_t pid;
-    int fd_in;
-    int fd_out;
-    char *cmd_path;
-} t_executor;
-
-typedef struct s_pipeline
-{
-    int nb_cmd;
-    int fd_prev;
-    int fd_next;
-    int fd[2];
-    t_ast **cmd_list;
-    pid_t *pids;
-} t_pipeline;
-
 # include <unistd.h>
 # include "minishell.h"
 # include "parser.h"
 # include "lexer.h"
 
-int     executor(t_mms *mms, t_ast *head);
+/* No struct changes were needed for the fixes applied in executor.c: */
+/* the leak, the write()/waitpid() checks, the 126/127 distinction,   */
+/* and moving builtins out of the fork are all internal to the .c.    */
+
+typedef struct s_executor
+{
+	pid_t	pid;
+	int		fd_in;
+	int		fd_out;
+	char	*cmd_path;
+}	t_executor;
+
+typedef struct s_pipeline
+{
+	int		nb_cmd;
+	int		fd_prev;
+	int		fd_next;
+	int		fd[2];
+	t_ast	**cmd_list;
+	pid_t	*pids;
+}	t_pipeline;
+
+int	executor(t_mms *mms, t_ast *head);
 
 #endif
