e9f7cf12b138f71c8c18464dc2e2c632ef6cb82a
fiaudfiz <michelouali0@gmail.com>
2026-07-24 15:08:30 +0200

    feat(main): new fonction converter

--- src/env/converter.c
+++ src/env/converter.c
@@ -6,7 +6,7 @@
 /*   By: fiaudfiz <fiaudfiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 14:01:35 by gd-hallu          #+#    #+#             */
-/*   Updated: 2026/07/24 10:51:31 by fiaudfiz         ###   ########.fr       */
+/*   Updated: 2026/07/24 15:02:50 by fiaudfiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,45 @@ int	env_arr_to_ht(char **envp, t_env *ht)
 		i++;
 	}
 	return (1);
+}
+
+/**
+ * @brief Converts the environment hashtable into a char** envp array.
+ *
+ * Only exported entries are included, skipping deleted slots.
+ *
+ * @param env Environment hashtable.
+ * @return Newly allocated NULL-terminated array of "KEY=VALUE" strings,
+ *         or NULL on allocation failure.
+ */
+char	**env_to_envp(t_env *env)
+{
+	char	**envp;
+	size_t	i;
+	size_t	j;
+	char	*line;
+
+	envp = malloc(sizeof(char *) * (env->entries + 1));
+	if (!envp)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < env->capacity)
+	{
+		if (env->indexes[i].key != NULL && env->indexes[i].key != DELETED
+			&& (env->indexes[i].flags & EXPORTED))
+		{
+			line = ft_strjoin(env->indexes[i].key, "=");
+			if (!line)
+				return (free(envp), NULL);
+			envp[j] = ft_strjoin(line, env->indexes[i].value);
+			free(line);
+			if (!envp[j])
+				return (free(envp), NULL);
+			j++;
+		}
+		i++;
+	}
+	envp[j] = NULL;
+	return (envp);
 }
