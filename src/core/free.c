e685cc7cb6057e5bc058b249bddea19949a8861b
fiaudfiz <michelouali0@gmail.com>
2026-07-24 11:48:21 +0200

    fix(main): ajout de fonction free_stack_alloc, reset_satck_alloc and free_node_ast

--- src/core/free.c
+++ src/core/free.c
@@ -3,14 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gd-hallu <gd-hallu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fiaudfiz <fiaudfiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 14:43:40 by gd-hallu          #+#    #+#             */
-/*   Updated: 2026/06/11 15:57:37 by gd-hallu         ###   ########.fr       */
+/*   Updated: 2026/07/24 11:36:12 by fiaudfiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	stack_reset(t_stack_alloc *sa)
+{
+	sa->up = sa->buffer;
+	sa->curr = NULL;
+}
+
+void	free_ast_values(t_ast *node)
+{
+	t_redir	*redir;
+	int		i;
+
+	if (!node)
+		return ;
+	if (node->type == NODE_CMD)
+	{
+		i = 0;
+		while (node->tokens[i])
+		{
+			free(node->tokens[i]->value);
+			i++;
+		}
+		redir = node->redirect;
+		while (redir)
+		{
+			free(redir->file);
+			redir = redir->next;
+		}
+	}
+	else
+	{
+		free_ast_values(node->left);
+		free_ast_values(node->right);
+	}
+}
+
+void	free_stack_allocator(t_stack_alloc *sa)
+{
+	if (!sa)
+		return ;
+	free(sa->buffer);
+	free(sa);
+}
 
 void    free_og_struct(t_mms *mms)
 {
@@ -19,6 +62,8 @@ void    free_og_struct(t_mms *mms)
     free_hash_table(mms->alias);
     free_hash_table(mms->cmd_path);
     free_env(mms->env);
+    free_stack_allocator(mms->sa);
+	free(mms->cwd); 
     free(mms->name);
     free(mms);
 }
