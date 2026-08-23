fac1762002b92824785c90c9dda75f2b9a376b56
Safe-net-user <gaspard.dhalluin@icloud.com>
2026-08-23 20:16:32 +0200

    fields supp

--- src/core/free.c
+++ src/core/free.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miouali <miouali@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gd-hallu <gd-hallu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 14:43:40 by gd-hallu          #+#    #+#             */
-/*   Updated: 2026/08/23 18:32:13 by miouali          ###   ########.fr       */
+/*   Updated: 2026/08/23 20:15:09 by gd-hallu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,16 +48,15 @@ void	free_stack_allocator(t_stack_alloc *sa)
 
 void	free_og_struct(t_mms *mms)
 {
-	if (!mms)
-		return ;
-	free_hash_table(mms->alias);
-	free_hash_table(mms->cmd_path);
-	free_env(mms->env);
-	free_stack_allocator(mms->sa);
-	free(mms->cwd);
-	free(mms->name);
-	free(mms->history_buffer);
-	free(mms);
+    if (!mms)
+        return ;
+    free_hash_table(mms->cmd_path);
+    free_env(mms->env);
+    free_stack_allocator(mms->sa);
+    free(mms->cwd);
+    free(mms->name);
+    free(mms->history_buffer);
+    free(mms);
 }
 
 int	cleanup_and_exit(t_mms *mms)
